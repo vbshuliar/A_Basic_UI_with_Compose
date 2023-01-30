2dc53b4933e768343b5c704f59452ef41b8172bd
vbshuliar <vbshuliar@gmail.com>
2023-01-30 01:59:30 +0000

    feat: successfully finished exercise

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,41 +19,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LittleLemonTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    RestaurantName(name = stringResource(id = R.string.title))
+                    RestaurantName(name = stringResource(id = R.string.title), size = 100)
                 }
             }
         }
     }
 }
-@Composable
-fun RestaurantName(name: String){
-    Text(text = name)
-}
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun RestaurantName(name: String, size: Int) {
+    Text(text = name, fontSize = size.sp)
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    LittleLemonTheme {
-//        Greeting("Android")
-//    }
-//}
 
 
 @Preview(showBackground = true)
 @Composable
-fun RestaurantNamePreview(){
+fun RestaurantNamePreview() {
     LittleLemonTheme {
-        RestaurantName(name = stringResource(id = R.string.title))
+        RestaurantName(name = stringResource(id = R.string.title), size = 32)
     }
 
 }
